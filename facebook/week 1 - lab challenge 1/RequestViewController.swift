fe66ea1853cc2d278a9f2558cf257f2bcb12eeb3
Dannyboy414 <dannyboy414@gmail.com>
2016-03-06 11:12:36 -0800

    Initial commit

---
+++ facebook/week 1 - lab challenge 1/RequestViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RequestViewController.swift
+//  Week 1 - Lab Challenge 1
+//
+//  Created by Alvin Hsia on 2/3/16.
+//  Copyright © 2016 Alvin Hsia. All rights reserved.
+//
+
+import UIKit
+
+class RequestViewController: UIViewController {
+    @IBOutlet weak var RequestScrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBarHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
